5a2a172710beb313d0dce309eecaaf242f7bc9cf
Mohit Pratap Singh <er.mohitpratapsingh@gmail.com>
2026-03-07 16:50:56 +0530

    Capture events of window read operations. And add font to Window.

--- SampleAppMemebers/gui.py
+++ SampleAppMemebers/gui.py
@@ -4,10 +4,14 @@ import FreeSimpleGUI as sg
 
 label = sg.Text("Type a member name")
 
-input_box = sg.InputText(tooltip="Enter member")
+input_box = sg.InputText(tooltip="Enter member", key = "Member")
 
 button = sg.Button("Add")
 
-window = sg.Window("My Members App", layout=[[label], [input_box, button]])
-window.read()
+window = sg.Window("My Members App",
+                   layout=[[label], [input_box, button]],
+                   font=("Helvetica", 12))
+
+event = window.read()
+print(event)
 window.close()
